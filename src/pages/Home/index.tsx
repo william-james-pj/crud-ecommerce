7df781bfd43aab26e3a15b65623575d927c4b75a
William James <william.james.pj@gmail.com>
2021-07-23 09:15:03 -0300

    Created home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
+import { Box } from "../../components/Box";
+import { Title } from "../../components/Title";
+
 import * as S from "./styles";
 
 export function Home() {
   return (
     <S.Container>
-      <h1>Home</h1>
+      <Title text="Home" />
+      <Box textHeader="Basic CRUD">
+        <S.Text>Welcome</S.Text>
+        <S.Text>Use the menu to access client and product features</S.Text>
+      </Box>
     </S.Container>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,12 @@
 import styled from "styled-components";
-// import * as fonts from '../../config/fonts';
+import * as fonts from "../../config/fonts";
 
 export const Container = styled.div``;
+
+export const Text = styled.p`
+  font-size: ${fonts.normal};
+  color: ${(props) => props.theme.colors.text};
+  & + & {
+    margin-top: 1rem;
+  }
+`;
